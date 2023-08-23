c563e7ab7c87e4596afd4b4d84bb236e2ef7f306
Melvin <melvingeorge6600@gmail.com>
2023-08-23 15:28:34 -0700

    initial

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+// function displayNum(num){
+//     result.value+=num
+// }
+// function clearbox(){
+//     result.value=""
+// }
+// function evaluateExpression(){
+//     result.val=eval(result.value)
+// }
+
+
+
 function displayNum(num){
     result.value+=num
-}
-function clearbox(){
-    result.value=""
-}
-function evaluateExpression(){
-    result.value=eval(result.value)
-}
+    
+    }
+    
+    function clearbox(){
+        result.value=""
+    }
+    function evaluateExpression(){
+        result.value=eval(result.value)
+    }
